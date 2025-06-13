daf121cc45c29785c868217c97bf1b6ac713db05
Altan <s0582356>
2025-06-13 03:56:05 +0200

    Last Meilenstein Backend. Frontend, Backend und die Datenbank bedienen sich mit Daten.

--- src/main/java/com/example/demo/Todo.java
+++ src/main/java/com/example/demo/Todo.java
@@ -4,56 +4,19 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 public class Todo {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    public Long id;
-    private String title;
-    private String description;
-    private boolean completed;
+    public long id;    // von Long auf long geändert! Primitive Datentypen sind mit typescript kompatibler
+    public String title;
+    public String description;
+    public boolean completed = false; // Direkt initialisiert
 
-    public Todo() {}
-
-    public Todo(String title, String description) {
-        this.title = title;
-        this.description = description;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getTitle() {
-        return title;
-    }
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
-
-    @Override
-    public String toString() {
-        return "Todo{" +
-                "id=" + id +
-                ", Titel='" + title + '\'' +
-                ", description=" + description +
-                '}';
-    }
 }
 
 
--- src/main/java/com/example/demo/TodoController.java
+++ src/main/java/com/example/demo/TodoController.java
@@ -1,29 +1,30 @@
 package com.example.demo;
 
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.logging.Logger;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
+@RequestMapping("/api")
 public class TodoController {
 
     @Autowired
-    TodoService service;
+    private TodoRepository repository;
 
+    @PostMapping("/todos")
+    public Todo addTodo(@RequestBody Todo todo) {
+        return repository.save(todo);
+    }
 
-    // auf localhost:8080/todos wird es angezeigt !
     @GetMapping("/todos")
-    public List<Todo> testRouter(){
-        return List.of(
-                new Todo("Hinzufügen", "Hier fügst du deine Aufgaben ein."),
-                new Todo("Löschen", "Hier löschst du deine Aufgaben."),
-                new Todo("Bearbeiten", "Hier kannst du deine Aufgaben bearbeiten."),
-                new Todo("Erinnern", "Hier wirst du an deinen Aufgaben erinnert."),
-                new Todo("Kategorisieren", "Hier kannst du deine Aufgaben Kategorisieren.")
-        );
+    public List<Todo> getTodos() {
+        return (List<Todo>) repository.findAll();
+    }
 
+    @DeleteMapping("/todos")
+    public void deleteAllTodos() {
+        repository.deleteAll();
     }
 }
