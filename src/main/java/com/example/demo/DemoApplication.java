f27de53f84e46216d1d5d92e03a59253463bcf5b
Altan <s0582356>
2025-06-27 20:11:44 +0200

    Testphase

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 // Das Backend zeigt die JSon-Daten auf Localhost:8080/todos an!
 
+// Backend zeigt die JSon-Daten die im Frontend erstellt werden auf http://localhost:8080/api/todos!
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
